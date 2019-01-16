30a93d5a71d1b7188b747b4c95116fa3cc7a4f39
Henrik Giesel <hengiesel@gmail.com>
2019-01-16 07:19:11 +0100

    Add nice icon

--- __init__.py
+++ __init__.py
@@ -16,6 +16,11 @@ from subprocess import Popen, PIPE
 # a = aqt.editcurrent.EditCurrent(mw).editor.note.values()
 config = mw.addonManager.getConfig(__name__)
 
+addon_path = os.path.dirname(__file__)
+
+icon_path = os.path.join(addon_path, "icons")
+icon_path_archive = os.path.join(icon_path, "archive.png")
+
 # cross out the currently selected text
 def on_archive(editor):
     found_file = False
@@ -24,37 +29,51 @@ def on_archive(editor):
     quest_content = editor.note.fields[quest_index]
     quest = re.search(config['quest_regex'], quest_content).group(1) # only match inner group
 
-    indices = [i for i, item in enumerate(editor.note.tags) if re.search('.*::.*', item)]
+    # first tag that is found that contains a sign of being hierarchical is taken to be topic
+    indices = [i for i, item in enumerate(editor.note.tags) if re.search('::', item)]
+
     if len(indices) >= 0:
         tag = editor.note.tags[indices[0]]
 
-        topic = re.search("([^:]*)::[^:]*", tag).group(1)
-        subtopic = re.search("::([^:]*)", tag).group(1)
+        tag_matches = re.search(config['topic_regex'], tag)
+        topic = tag_matches.group(1)
+        subtopic = tag_matches.group(2)
+
+        if config['debug']:
+            showInfo(str(
+                '$TOPIC    = ' + topic    + '\n' +
+                '$SUBTOPIC = ' + subtopic + '\n' +
+                '$QUEST    = ' + quest    + '\n'
+                ))
 
         for root, _, files in os.walk(config['notes_path']):
             if os.path.basename(root) == topic:
-                for file in files:
-                    if re.match(subtopic, file):
+                for filename in files:
+                    if re.match(subtopic, filename):
 
                         editor_command = config['editor_command']
 
+                        lineno='1'
+                        with open(root+'/'+filename, 'r+', encoding='utf-8') as handler:
+                            for num, line in enumerate(handler, 1):
+                                if re.match(re.sub('\([^?].*\)', quest, config['quest_regex']), line):
+                                    lineno=str(num)
+
                         if config['debug']:
                             showInfo(str(
-                                '$TOPIC' + topic + '\n' +
-                                '$ROOT' + root + '\n' +
-                                '$SUBTOPIC' ++ subtopic + '\n' +
-                                '$FILE' + file + '\n' +
-                                '$QUEST' + quest + '\n'
+                                '$ROOT   = ' + root     + '\n' +
+                                '$FILE   = ' + filename + '\n' +
+                                '$LINENO = ' + lineno   + '\n'
                                 ))
-
 
                         proc = Popen(
                             [re.sub('\$TOPIC', topic, v) for v in
                              [re.sub('\$ROOT', root, v) for v in
                               [re.sub('\$SUBTOPIC', subtopic, v) for v in
-                               [re.sub('\$FILE', file, v) for v in
+                               [re.sub('\$FILE', filename, v) for v in
                                 [re.sub('\$QUEST', quest, v) for v in
-                                 editor_command]]]]], env={})
+                                 [re.sub('\$LINENO', lineno, v) for v in
+                                  editor_command]]]]]], env={})
                         found_file = True
 
     if not found_file:
@@ -64,7 +83,7 @@ def on_archive(editor):
 def add_my_button(buttons, editor):
     editor._links['archive'] = on_archive
     buttons.insert(-1, editor._addButton(
-        "iconname", # "/full/path/to/icon.png",
+        icon_path_archive, # "/full/path/to/icon.png",
         "archive",
         "Open quest in archive"))
     return buttons
--- config.md
+++ config.md
@@ -1,7 +1,7 @@
-
-- in `editor_command`:
-  * `$TOPIC` expands to topic
-  * `$ROOT` expands to expanded topic directory
-  * `$SUBTOPIC` expands to subtopic
-  * `$FILE` expands to basename of expanded subtopic
-  * `$QUEST` expands to quest
+### Meaning of variables in `editor_command`
+- `$TOPIC` expands to topic
+- `$ROOT` expands to expanded topic directory
+- `$SUBTOPIC` expands to subtopic
+- `$FILE` expands to basename of expanded subtopic
+- `$QUEST` expands to quest
+- `$LINENO` expands to line number of quest tag
