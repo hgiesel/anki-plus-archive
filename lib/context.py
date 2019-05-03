56875fcc7909959cf91124d6a7e33a32fb95fb0a
Henrik Giesel <hengiesel@gmail.com>
2019-05-03 21:43:10 +0200

    Implement clipboard function

--- lib/context.py
+++ lib/context.py
@@ -12,133 +12,122 @@ from .util import *
 
 from . import pyperclip
 
-def on_archive(editor, config, comm):
-    found_file = False
-    qid_field_name = config['card_sets'][0]['qid_field']
+def on_command_replace(text, archive_root, section, page, qid):
+    for root, _, files in os.walk(archive_root):
+        if os.path.basename(root) == section:
+            for filename in files:
+                if re.match(page, filename):
 
-    try:
-        qid_field = editor.note.keys().index(qid_field_name)
-    except:
-        showInfo('Note type does not have quest field: ' + qid_field_name)
-    return
+                    return list(map(
+                            lambda x: re.sub(r'$ROOT', archive_root, x), map(
+                                lambda x: re.sub(r'$FILE', filename, x), map(
+                                    lambda x: re.sub(r'$SECTION', section, x), map(
+                                        lambda x: re.sub(r'$PAGE', page, x), map(
+                                            lambda x: re.sub(r'$QID', qid, x), text))))))
 
-    if qid_field:
-        qid = editor.note.fields[qid_field]
+def on_command(editor, archive_root: str, card_sets, comm) -> None:
+    ### get qid
+    qid_field_name = card_sets[0]['qid_field']
+
+    if qid_field_name:
+        try:
+            qid_field = editor.note.keys().index(qid_field_name)
+        except:
+            showInfo('Note type does not have quest field: ' + qid_field_name)
+            return None
+
+        qid_unclean = editor.note.fields[qid_field]
         qid_regex = re.compile("^:?([0-9]+):?")
 
-    try:
-        quest = re.search(qid_regex, qid).group(1) # only match inner group
-    except:
-        showInfo('Quest field "'+ config['card_sets'][0]['qid_field'] +'" does not contain quest tag specified by this regex: "'
-                + "^:([0-9]+):(?: .*)?" + '"')
-        return
-
+        try:
+            qid = re.search(qid_regex, qid_unclean).group(1) # only match inner group
+        except:
+            showInfo('Quest field "'+ card_sets[0]['qid_field'] +'" does not contain quest tag specified by this regex: "'
+                    + "^:([0-9]+):(?: .*)?" + '"')
+            return None
     else:
-        quest = str(editor.note.id)
+        qid = str(editor.note.id)
 
-    pageid_prefix = config['card_sets'][0]['pageid_prefix']
+    ### get section and page
+    pageid_prefix = card_sets[0]['pageid_prefix']
     pageid_regex = re.compile((pageid_prefix + "::" if pageid_prefix else "") + "(?:.*::)?([^:]*)::([^:]*)$")
 
     # first tag that is found that contains a sign of being hierarchical is taken to be section
     indices = [i for i, item in enumerate(editor.note.tags) if pageid_regex.search(item)]
 
     if len(indices) == 0:
-        showInfo('Tags do not contain section tag')
-    return
+        showInfo('Tags do not contain section tag!')
+        return None
 
-    if len(indices) > 1 :
-        showInfo('Tags contain multiple viable pageids')
-    return
+    elif len(indices) != 1:
+        showInfo('''
+        Tags contain multiple viable pageids.
+        Only one tag is allowed to be hierarchical!')
+        '''.lstrip())
+        return None
 
     tag = editor.note.tags[indices[0]]
     tag_matches = pageid_regex.search(tag)
+
     section = tag_matches.group(1)
     page = tag_matches.group(2)
 
-#     if config['debug']:
-#         showInfo(str(
-#             '$SECTION    = ' + section    + '\n' +
-#             '$PAGE = ' + page + '\n' +
-#             '$QUEST    = ' + quest    + '\n'
-#             ))
+    # showInfo("%s::%s#%s" % (section, page, qid))
 
-    for root, _, files in os.walk(config['archive_root']):
-        if os.path.basename(root) == section:
-            for filename in files:
-                if re.match(page, filename):
-                    if comm['type'] == 'shell':
-                        shell_command = comm['arguments']
+    if comm['type'] == 'shell':
+        result = on_command_replace(comm['arguments'], archive_root, section, page, qid)
 
-            proc = Popen(
-                    map(lambda x: re.sub(r'$SECTION', section, x),
-                        map(lambda x: re.sub(r'$ROOT', root, x),
-                            map(lambda x: re.sub(r'$PAGE', page, x),
-                                map(lambda x: re.sub(r'$FILE', filename, x),
-                                    map(lambda x: re.sub(r'$QUEST', quest, x),
-                                        # map(lambda x: re.sub('\$LINENO', lineno, x),
-                                        shell_command))))), env={})
+    elif comm['type'] == 'clipboard':
+        result = on_command_replace([comm['text']], archive_root, section, page, qid)[0]
+        pyperclip.copy(result)
 
-            found_file = True
-
-        elif comm['type'] == 'clipboard':
-
-            pyperclip.copy(
-                re.sub(r'$SECTION', section,
-                    re.sub(r'$ROOT', root,
-                        re.sub(r'$PAGE', page,
-                            re.sub(r'$FILE', filename,
-                                re.sub(r'$QUEST', quest,
-                                    # re.sub('\$LINENO', lineno,
-                                    comm['text']))))))
-
-            found_file = True
-
-
-    if not found_file:
-        showInfo('Nothing was found')
+    else:
+        showInfo('Invalid command type!')
+        return
 
 
 def main(config, icons):
 
-    def on_archive0(editor):
+    def on_command0(editor):
         nonlocal config
-        on_archive(editor, config, config['commands'][0])
-    def on_archive1(editor):
+        showInfo('Nothing was found')
+        on_command(editor, config['archive_root'], config['card_sets'], config['commands'][0])
+    def on_command1(editor):
         nonlocal config
-        on_archive(editor, config, config['commands'][1])
-    def on_archive2(editor):
+        on_command(editor, config['archive_root'], config['card_sets'], config['commands'][1])
+    def on_command2(editor):
         nonlocal config
-        on_archive(editor, config, config['commands'][2])
-    def on_archive3(editor):
+        on_command(editor, config['archive_root'], config['card_sets'], config['commands'][2])
+    def on_command3(editor):
         nonlocal config
-        on_archive(editor, config, config['commands'][3])
-    def on_archive4(editor):
+        on_command(editor, config['archive_root'], config['card_sets'], config['commands'][3])
+    def on_command4(editor):
         nonlocal config
-        on_archive(editor, config, config['commands'][4])
-    def on_archive5(editor):
+        on_command(editor, config['archive_root'], config['card_sets'], config['commands'][4])
+    def on_command5(editor):
         nonlocal config
-        on_archive(editor, config, config['commands'][5])
+        on_command(editor, config['archive_root'], config['card_sets'], config['commands'][5])
 
-    on_archive_commands = [
-        on_archive0,
-        on_archive1,
-        on_archive2,
-        on_archive3,
-        on_archive4,
-        on_archive5,
+    on_command_list = [
+        on_command0,
+        on_command1,
+        on_command2,
+        on_command3,
+        on_command4,
+        on_command5,
     ]
 
     def add_my_buttons(buttons, editor):
         nonlocal config
         nonlocal icons
-        nonlocal on_archive_commands
+        nonlocal on_command_list
 
-        for i, _command in enumerate(config['commands']):
-            editor._links['archive'] = on_archive_commands[i]
+        for i, command in enumerate(config['commands']):
+            editor._links[command['title']] = on_command_list[i]
             buttons.insert(-1, editor._addButton(
                 icons[i],
-                "command " + str(i),
-                "Command " + str(i) +" for archive"
+                command['title'],
+                command['description'],
                 ))
 
         return buttons
